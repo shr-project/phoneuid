36156f875b8b4391bc52df73f6531488b0b3e1b5
Klaus Kurzmann <mok@fluxnetz.de>
2009-10-17 09:21:45 +0200

    drop ophonekitd stuff
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/ophonekitd/ophonekitd-dbus-common.h
+++
@@ -1,7 +0,0 @@
-#ifndef _OPHONEKITD_DBUS_COMMON_H
-#define _OPHONEKITD_DBUS_COMMON_H
-
-#define OPHONEKITD_USAGE_SERVICE_PATH "/org/shr/ophonekitd/Usage"
-#define OPHONEKITD_USAGE_SERVICE_NAME "org.shr.ophonekitd.Usage"
-
-#endif
