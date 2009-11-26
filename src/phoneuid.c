fdc6be45af06efabf8bfa838391d2c16ac7990c2
Tom Hacohen <tom@nospam>
2009-11-26 03:40:02 +0200

    Added config file and support for different log levels (set from config). ATM default log level is DEBUG, should probably change to INFO

--- src/phoneuid.c
+++ src/phoneuid.c
@@ -11,6 +11,7 @@
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-bindings.h>
 #include <phoneui/phoneui.h>
+#include <time.h>
 #include "phoneuid-call-management.h"
 #include "phoneuid-dialer.h"
 #include "phoneuid-notification.h"
@@ -19,6 +20,11 @@
 #include "phoneuid-dbus-common.h"
 
 static FILE *logfile = NULL;
+/*FIXME: hardcoded, shoudl change */
+#define CONF_FILE "/etc/phoneui.conf"
+#define LOGFILE "/var/log/phoneuid.log"
+#define DEFAULT_DEBUG_LEVEL "INFO"
+static GLogLevelFlags log_flags;
 
 static void
 _log_handler(const gchar *domain, GLogLevelFlags level, const gchar *message,
@@ -27,13 +33,126 @@ _log_handler(const gchar *domain, GLogLevelFlags level, const gchar *message,
 	char date_str[30];
 	struct timeval tv;
 	struct tm ptime;
+	char *levelstr;
+	if (!(log_flags & G_LOG_LEVEL_MASK & level)) {
+		return;
+	}
 	gettimeofday(&tv, NULL);
 	localtime_r(&tv.tv_sec, &ptime);
 
 	strftime(date_str, 30, "%Y.%m.%d %T", &ptime);
 
-	fprintf(logfile, "%s.%06d [%s]\t %s\n", date_str, tv.tv_usec, domain, message);
+	switch (level) {
+	case G_LOG_LEVEL_ERROR:
+		levelstr = "ERROR";
+		break;
+	case G_LOG_LEVEL_CRITICAL:
+		levelstr = "CRITICAL";
+		break;
+	case G_LOG_LEVEL_WARNING:
+		levelstr = "WARNING";
+		break;
+	case G_LOG_LEVEL_MESSAGE:
+		levelstr = "MESSAGE";
+		break;
+	case G_LOG_LEVEL_INFO:
+		levelstr = "INFO";
+		break;
+	case G_LOG_LEVEL_DEBUG:
+		levelstr = "DEBUG";
+		break;
+	default:
+		levelstr = "";
+		break;
+	}
+
+	fprintf(logfile, "%s.%06d [%s]\t%s: %s\n", date_str, tv.tv_usec,
+			domain, levelstr, message);
 	fflush(logfile);
+}
+static void
+_empty_log_handler(const gchar *domain, GLogLevelFlags level, const gchar *message,
+		gpointer userdata)
+{
+	(void) domain;
+	(void) level;
+	(void) message;
+	(void) userdata;
+}
+static void
+_load_config()
+{
+	GKeyFile *keyfile;
+	GKeyFileFlags flags;
+
+	GError *error = NULL;
+	int failed = 0;
+	char *logpath = NULL;
+	char *debug_level = NULL;
+
+	/* Read the phonefsod preferences */
+	keyfile = g_key_file_new();
+	flags = G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS;
+	if (g_key_file_load_from_file
+	    (keyfile, CONF_FILE, flags, &error)) {
+		logpath = g_key_file_get_string(keyfile, "logging",
+					"log_file", NULL);
+		
+		debug_level = g_key_file_get_string(keyfile, "logging",
+					"log_level", NULL);
+		/* initialize logging */
+	}
+	else {
+		failed = 1;
+	}
+	debug_level = (debug_level) ? debug_level : DEFAULT_DEBUG_LEVEL;
+	logpath = (logpath) ? logpath : LOGFILE;
+	printf("Log file: %s\nLog level: %s\n", logpath, debug_level);
+	
+	logfile = fopen(logpath, "a");
+	if (!logfile) {
+		printf("Error creating the logfile (%s) !!!", LOGFILE);
+		return (-3);
+	}
+	
+	log_flags = G_LOG_FLAG_FATAL;
+	
+	if (!strcmp(debug_level, "DEBUG")) {
+		log_flags |= G_LOG_LEVEL_MASK;
+	}
+	else if (!strcmp(debug_level, "INFO")) {
+		log_flags |= G_LOG_LEVEL_MASK ^ (G_LOG_LEVEL_DEBUG);
+	}
+	else if (!strcmp(debug_level, "MESSAGE")) {
+		log_flags |= G_LOG_LEVEL_MASK ^ (G_LOG_LEVEL_DEBUG
+			| G_LOG_LEVEL_INFO);
+	}
+	else if (!strcmp(debug_level, "WARNING")) {
+		log_flags |= G_LOG_LEVEL_MASK ^ (G_LOG_LEVEL_DEBUG
+			| G_LOG_LEVEL_INFO | G_LOG_LEVEL_MESSAGE);
+	}
+	else if (!strcmp(debug_level, "CRITICAL")) {
+		log_flags |= G_LOG_LEVEL_MASK ^ (G_LOG_LEVEL_DEBUG
+			| G_LOG_LEVEL_INFO | G_LOG_LEVEL_MESSAGE
+			| G_LOG_LEVEL_WARNING);
+	}
+	else if (!strcmp(debug_level, "ERROR")) {
+		log_flags |= G_LOG_LEVEL_MASK ^ (G_LOG_LEVEL_DEBUG
+			| G_LOG_LEVEL_INFO | G_LOG_LEVEL_MESSAGE
+			| G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL);
+	}
+	else {
+	}
+	
+	
+	if (failed) {
+		g_message(error->message);
+		g_debug("Reading configuration file error, skipping");
+	}
+	else {
+		g_debug("Configuration file read");
+		g_message("Using log level '%s'", debug_level);
+	}
 }
 
 static gpointer
@@ -80,13 +199,9 @@ phoneuid_dbus_setup()
 int
 main(int argc, char **argv)
 {
-
-	logfile = fopen("/var/log/phoneuid.log", "a");
-	if (!logfile) {
-		printf("Error creating the logfile!!!");
-		return (-3);
-	}
+	g_log_set_fatal_mask(NULL, G_LOG_LEVEL_ERROR);
 	g_log_set_default_handler(_log_handler, NULL);
+	_load_config();
 	g_type_init();
 
 	if (!g_thread_supported())
