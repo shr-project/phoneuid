be0c9364a0b5e997be2151213249535d8e01f194
Tom 'TAsn' Hacohen <tom@nospam>
2010-03-01 12:50:45 +0200

    Changed the wrapper so normal exit won't trigger a reload

--- data/phoneui-wrapper.sh
+++ data/phoneui-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-CODE=0
-while [ $CODE -ne 128 -a $CODE -ne 137 -a $CODE -ne 255 ]
+CODE=-1
+while [ $CODE -ne 128 -a $CODE -ne 137 -a $CODE -ne 255 -a $CODE -ne 0 ]
 do
 	ELM_ENGINE=x11 phoneuid
 	CODE=$?
