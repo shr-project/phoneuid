28c1d594edbd5be93bb4add5812999bac3c3937f
Tom 'TAsn' Hacohen <tom@nospam>
2010-08-02 00:07:24 +0300

    Updated licensing information to GPLv2+. Thanks sre. Relates to ticket #1165.

--- src/phoneuid-dialer.h
+++ src/phoneuid-dialer.h
@@ -5,7 +5,7 @@
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Public License as published by
- *  the Free Software Foundation; version 2 of the license.
+ *  the Free Software Foundation; version 2 of the license or any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
