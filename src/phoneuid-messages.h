0810f55d08ff2f2e072bab20d5c323a331a445dc
Klaus Kurzmann <mok@fluxnetz.de>
2010-08-02 17:05:35 +0200

    add the license to _all_ source code files
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- src/phoneuid-messages.h
+++ src/phoneuid-messages.h
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (C) 2009
+ *      Authors (alphabetical) :
+ *              Klaus 'mrmoku' Kurzmann <mok@fluxnetz.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Public License as published by
+ *  the Free Software Foundation; version 2 of the license or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ */
+
 #ifndef _PHONEUID_MESSAGES_H
 #define _PHONEUID_MESSAGES_H
 
